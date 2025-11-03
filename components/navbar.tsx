e6fe98054a265af339163dcaf7c85fe8dc6e0d59
Umar-azeem <umara4436@gmail.com>
2025-11-03 19:34:15 +0500

    Initial commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -134,15 +134,17 @@ export default function Navbar() {
           </div>
 
           {/* Search Bar */}
-          <div className="hidden md:flex items-center gap-2">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="px-3 py-2 rounded bg-white text-gray-800 text-sm w-32 focus:outline-none"
-            />
-            <button className="bg-white text-[#2E5090] p-2 rounded hover:bg-gray-100 transition">
-              <Search size={18} />
-            </button>
+          <div className="hidden lg:flex items-center gap-2">
+             <Link href="/" className="flex items-center gap-2 font-bold text-xl ">
+            <div className="w-20 h-20 md:w-28 md:h-20 lg:w-36 lg:h-36 rounded-full flex items-center justify-center ">
+              <Image
+                src="/image/logo2.png"
+                alt="Example image"
+                width={500}
+                height={300}
+              />
+            </div>
+          </Link>
           </div>
 
           {/* Mobile Menu Button */}
